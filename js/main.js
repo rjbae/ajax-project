ea5c9de304f0ab818881c5db6856857a5a7bdc1b
Ryan Bae <baeryan21@gmail.com>
2022-06-17 16:27:15 +0000

    User can search for park names by state by using the drop down menu.

--- js/main.js
+++ js/main.js
@@ -1,88 +1,78 @@
 var $homeNav = document.querySelector('.nav-home');
 var $homePage = document.querySelector('.home-page');
 var $parksResult = document.querySelector('.parks-list');
-var $searchButton = document.querySelector('.search-button');
-// var $options = document.querySelector('option');
+var $form = document.querySelector('.search-bar');
+var $input = document.querySelector('.search-button');
+var $select = document.querySelector('.stateSelect');
 
-$homeNav.addEventListener('click', homeNavEvent);
+$homeNav.addEventListener('click', homeView);
 
-function homeNavEvent(event) {
+function parksView(event) {
+  $homePage.className = 'home-page hidden';
+  $parksResult.className = 'parks-list';
+  data.view = 'parks-result';
+}
+
+function homeView(event) {
   $homePage.className = 'home-page';
   $parksResult.className = 'parks-list hidden';
+  data.view = 'home-page';
 }
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://developer.nps.gov/api/v1/parks?limit=466&start=0&q=parks&api_key=1tk21xLEsdglEhmTVa3WKMvdsPPTz04Mrgd4F9pw');
 xhr.responseType = 'json';
 
-xhr.addEventListener('load', xhrEvent);
-function xhrEvent(event) {
-  var parks = xhr.response;
-  // console.log(parks);
-  for (var i = 0; i < parks.data.length; i++) {
-    var parkInfo = parks.data[i];
-    var parkName = parkInfo.fullName;
-    var parkImg = parkInfo.images[0].url;
-    var parkDescription = parkInfo.description;
-    var parkState = parkInfo.states;
+function getState(name) {
+  xhr.open('GET', 'https://developer.nps.gov/api/v1/parks?limit=466&start=0&q=' + name + '&api_key=1tk21xLEsdglEhmTVa3WKMvdsPPTz04Mrgd4F9pw');
+  xhr.send();
+  xhr.addEventListener('load', function () {
+    var parks = xhr.response;
+    var $parksList = document.querySelector('.parks-result');
 
-    var $parkList = document.createElement('li');
+    for (var i = 0; i < parks.data.length; i++) {
+      var park = parks.data[i];
+      var $parkList = document.createElement('li');
 
-    var $columnDiv = document.createElement('div');
-    $columnDiv.setAttribute('class', 'column-half');
-    $parkList.appendChild($columnDiv);
+      var $row = document.createElement('div');
+      $row.setAttribute('class', 'row');
+      $parkList.appendChild($row);
 
-    var $image = document.createElement('img');
-    $image.setAttribute('src', parkImg);
-    $image.setAttribute('class', 'park-img');
-    $columnDiv.appendChild($image);
+      var $firstColumn = document.createElement('div');
+      $firstColumn.setAttribute('class', 'column-half');
+      $row.appendChild($firstColumn);
 
-    var $parkName = document.createElement('a');
-    $parkName.setAttribute('href', parkState);
-    $parkName.setAttribute('class', 'state-link');
-    $parkName.textContent = parkName;
-    $columnDiv.appendChild($parkName);
+      var $image = document.createElement('img');
+      $image.setAttribute('src', park.images[0].url);
+      $image.setAttribute('class', 'park-img');
+      $firstColumn.appendChild($image);
 
-    var $parkDescription = document.createElement('p');
-    $parkDescription.textContent = parkDescription;
-    $columnDiv.appendChild($parkDescription);
+      var $parkName = document.createElement('a');
+      $parkName.setAttribute('href', '');
+      $parkName.setAttribute('class', 'state-link');
+      $parkName.textContent = park.fullName;
+      $firstColumn.appendChild($parkName);
 
-    $ul.appendChild($parkList);
-  }
-}
-xhr.send();
+      var $secondColumn = document.createElement('div');
+      $secondColumn.setAttribute('class', 'column-half');
+      $row.appendChild($secondColumn);
 
-var $ul = document.querySelector('.parks-result');
+      var $parkDescription = document.createElement('p');
+      $parkDescription.textContent = park.description;
+      $secondColumn.appendChild($parkDescription);
 
-// var $dropDown = document.querySelector('.state-dropdown');
-
-$searchButton.addEventListener('click', renderSearch);
-// $dropDown.addEventListener('click', renderSearch);
-
-function renderSearch(event) {
-  var parks = xhr.response;
-  for (var i = 0; i < parks.data.length; i++) {
-    // var parkInfo = parks.data[i];
-    // var parkName = parkInfo.fullName;
-    // var parkImg = parkInfo.images[0].url;
-    // var parkDescription = parkInfo.description;
-    // var parkState = parkInfo.states;
-    $homePage.className = 'home-page hidden';
-    $parksResult.className = 'parks-list';
-    var list = [];
-    var select = document.querySelector('.state-dropdown');
-    for (var j = 1; j < select.length; j++) {
-      var swag = select.options[j].value;
-      list.push(swag);
+      $parksList.appendChild($parkList);
     }
-  }
+  });
 }
-// function returnThis() {
-//   var list = [];
-//   var select = document.querySelector('.state-dropdown');
-//   for (var j = 1; j < select.length; j++) {
-//     var swag = select.options[j].value;
-//     list.push(swag);
-//   }
-//   return list;
-// }
+
+$input.addEventListener('click', formEvent);
+
+var parksHeader = document.querySelector('.result-header');
+function formEvent(event) {
+  event.preventDefault();
+  parksView();
+  var selectState = $select.value;
+  parksHeader.textContent = 'Parks: ' + selectState;
+  getState(selectState);
+  $form.reset();
+}
